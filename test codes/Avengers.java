709e4b2822f432c1e2c2ef9d5074f611e6c6ee4e
Kristel Marie P. Olaco <77267245+kristelmarie@users.noreply.github.com>
2022-09-15 21:31:56 +0800

    Create Avengers.java

---
+++ test codes/Avengers.java
@@ -0,0 +1,15 @@
+public class Avengers {
+    public static void main(String[] args) {
+
+        Character telepath = new Character();
+
+        telepath.strength = "mind reading";
+        telepath.name = "Charles Xavier or also known as Professor X";
+        telepath.txt = "Erik Lehnsherr";
+        telepath.sayMyName();
+        telepath.sayMyStrength();
+        telepath.sayMyTxtxt();
+        
+    }
+
+}
