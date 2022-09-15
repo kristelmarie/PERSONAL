e68b6e4964bddbb2c356b2449ce584f346b433d4
Kristel Marie P. Olaco <77267245+kristelmarie@users.noreply.github.com>
2022-09-15 21:41:04 +0800

    Create Pet.java

---
+++ week3/Pet.java
@@ -0,0 +1,12 @@
+public class Pet {
+    String name;
+    int trustlevel;
+    int petlevel;
+    boolean ishappy;
+
+    public void petDescription () {
+        System.out.println("My name is " + name + " and the level of my trust level is " + trustlevel);
+
+    }
+
+}
