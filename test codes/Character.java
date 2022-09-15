3c13f986124d4cff55049e94e48b57490ef332c9
Kristel Marie P. Olaco <77267245+kristelmarie@users.noreply.github.com>
2022-09-15 21:32:27 +0800

    Create Character.java

---
+++ test codes/Character.java
@@ -0,0 +1,21 @@
+public class Character {
+    String strength;
+    String name;
+    String txt;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+    
+    public void sayMyStrength() {
+        System.out.println("My strength is " + strength);
+
+    }
+
+    public static void sayMyTxt() {
+        System.out.println("Ny partner is " + txt);
+
+    }
+
+    
+    }
