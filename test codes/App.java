70410091e71ff6b19a541c97b1147e9e3fbc709b
Kristel Marie P. Olaco <77267245+kristelmarie@users.noreply.github.com>
2022-10-17 15:35:50 +0800

    Create App.java

---
+++ test codes/App.java
@@ -0,0 +1,52 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        // int counter = 5;
+
+        // Print your name 5 times (WHILE LOOP)
+        // while(counter != 0) {
+           // System.out.println("Elizer");
+            //System.out.println("Counter: " + counter);
+            //counter --;
+        //}
+
+        // Print your name 5 times (DO-WHILE LOOP)
+        //do {
+          //  System.out.println("Elizer");
+        //} while (counter != 5);
+
+        // Print your name 5 times (FOR LOOP)
+        //for (int counter = 5; counter != 0; counter--) {
+          //  System.out.println("Elizer");
+        //}
+
+
+        // Drinking game (FOR LOOP)
+        //for (int i = 5; i != 0; i--) {
+          //  if (i == 5) {
+            //    System.out.println("Elizer ");
+            //} else if (i == 4) {
+              //  System.out.println("Robbie ");
+            //} else if (i == 3) {
+            //    System.out.println("Allen ");
+            //} else if (i == 2) {
+              //  System.out.print("Mureigne ");
+                //System.out.println("Pass muna ako! ");
+                //continue;
+            //} else if (i == 1) {
+              //  System.out.println("Francyne ");
+            //}
+
+            //System.out.println("Shot!");
+        //}
+
+        
+    }
+            
+
+       
+    }
+
+    
+
