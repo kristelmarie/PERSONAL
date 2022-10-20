03968d1c39fb1e109a7c2f25edbe8b71f9c9f1e5
Kristel Marie P. Olaco <77267245+kristelmarie@users.noreply.github.com>
2022-10-20 15:46:22 +0800

    Create loop.java

---
+++ test codes/loop.java
@@ -0,0 +1,20 @@
+public class loop {
+    public static void main(String[] args) {
+      String[][] box =
+
+            {{"A", "B", "C"},
+    
+            {"D", "E", "F"},
+    
+            {"G", "H", "I"}};
+
+  for (int row = 0; box.length > box; box++) {
+      for (int column = 0; box.length > column; column++) {
+          System.out.print(box[row][column]);
+      }
+      System.out.println(" ");
+            }
+
+             
+    }
+}
