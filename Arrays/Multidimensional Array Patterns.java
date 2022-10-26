33e9970bddb7b8d5e080a41de61b83d7c67423e2
Kristel Marie P. Olaco <77267245+kristelmarie@users.noreply.github.com>
2022-10-26 20:39:41 +0800

    Create Multidimensional Array Patterns.java

---
+++ Arrays/Multidimensional Array Patterns.java
@@ -0,0 +1,53 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        char [][] array_3x3 = {{'A','B','C'},{'D','E','F'},{'G','H','I'}};
+    
+        // // A TO I
+        // print ABC DEF GHI
+        for (int rows = 0; rows < array_3x3.length; rows++) {
+            
+            System.out.println("");
+            for (int columns = 0; columns < array_3x3.length; columns++) {
+                System.out.print(array_3x3[rows][columns]);
+            }
+        }
+
+        // I TO A
+        // print GHI DEF ABC
+        for (int rows = array_3x3.length - 1; rows >= 0; rows--) {
+            
+            System.out.println("");
+
+            for (int columns = 0 ; columns < array_3x3.length; columns++) {
+
+                System.out.print(array_3x3[rows][columns]);
+            }
+        }
+
+        // Pattern
+        // Print column 0 and column 2 if the row is even.
+        // Print column 1 if the row is odd.
+        
+        for (int rows = 0; rows < array_3x3.length; rows++) {
+            
+            System.out.println("");
+
+            for (int columns = 0; columns < array_3x3.length; columns++) {
+
+                // EVEN ROWS
+                if (rows % 2 == 0 && (columns == 0 || columns == 2)) {
+                    System.out.print(array_3x3[rows][columns]);
+                }
+                // ODD ROWS
+                else if (rows % 2 != 0 && columns == 1) {
+                    System.out.print(array_3x3[rows][columns]);
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+        }
+    }
+}
